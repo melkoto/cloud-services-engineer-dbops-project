91d1e5a5489db3419bf402361a1fd45f4ab752fd
Movsar Makhmutov <m.movsar@gmail.com>
2025-03-16 15:50:09 +0000

    add flyway

--- migrations/V004__create_index.sql
+++ migrations/V004__create_index.sql
@@ -1,3 +1,7 @@
+-- Удаляем индекс, если он существует
+DROP INDEX IF EXISTS order_product_order_id_idx;
+DROP INDEX IF EXISTS orders_status_date_idx;
+
 -- Индекс для ускорения JOIN между order_product и orders по полю order_id
 CREATE INDEX order_product_order_id_idx ON order_product(order_id);
 
